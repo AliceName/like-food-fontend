efdc80be33ad3ed1e8ede6070575fb149a7a73ed
Ngà Trần <tablinh2020@gmail.com>
2026-04-23 23:44:39 +0700

    key db

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,8 +1,6 @@
-// File: src/supabaseClient.js
 import { createClient } from "@supabase/supabase-js";
 
-// THAY 2 DÒNG DƯỚI BẰNG THÔNG TIN BẠN VỪA COPY Ở BƯỚC 1
-const supabaseUrl = "https://cdewocwuuzryyzdxicol.supabase.co";
-const supabaseKey = "sb_publishable_XqqEgkBIzB3OR3q-ErI1aw_38aOaux8";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
